ef20a7ea99840a4390f1e33a0e9fa9b0ebd35bac
Stan Lipman <lipman.stan10@gmail.com>
2022-05-10 12:54:13 +0200

    added a comment and made a change to gitignore

--- HMI/src/com/tvis/Main.java
+++ HMI/src/com/tvis/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         //Frame frame = new Frame();\
+        // Ja dit is commentaar
         PickProces pickProces = new PickProces();
         PickProcesMonitor pickProcesMonitor = new PickProcesMonitor();
         MainFrame frame = new MainFrame(pickProces, pickProcesMonitor);
