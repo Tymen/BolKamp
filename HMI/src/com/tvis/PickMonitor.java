edef4bc47ac4572da12de643fda6fb7c683c2196
jacobjanwoord <jacobjanwoord@gmail.com>
2022-05-24 09:35:55 +0200

    added drawpath functie

--- HMI/src/com/tvis/PickMonitor.java
+++ HMI/src/com/tvis/PickMonitor.java
@@ -49,6 +49,8 @@ public class PickMonitor extends JPanel {
         }
 
         setProductStatus();
+
+        drawPath();
     }
 
     public void setProductStatus() {
@@ -58,6 +60,31 @@ public class PickMonitor extends JPanel {
             }
             updateStatus(productStatus[i], productenToBePicked.get(i));
         }
+    }
+
+    public void drawPath() {
+        for (int i = 0; i < productenToBePicked.size() - 1; i++) {
+            StorageBox storageBox1 = getStorageBox(productenToBePicked.get(i));
+            StorageBox storageBox2 = getStorageBox(productenToBePicked.get(i + 1));
+
+            canvas.setColor(Color.blue);
+            Graphics2D g2 = (Graphics2D) canvas;
+            g2.setStroke(new BasicStroke(10));
+
+            int x1 = storageBox1.getX() + ((canvasWidth - (sizeBetween * 4)) / 5) / 2;
+            int x2 = storageBox2.getX() + ((canvasWidth - (sizeBetween * 4)) / 5) / 2;
+            int y1 = storageBox1.getY() + ((canvasHeight - (sizeBetween * 4)) / 5) / 2;
+            int y2 = storageBox2.getY() + ((canvasHeight - (sizeBetween * 4)) / 5) / 2;
+
+            canvas.drawLine(x1, y1, x2, y2);
+            canvas.setColor(Color.red);
+            canvas.drawOval(x1 - 5, y1 - 5, 10, 10);
+        }
+
+        StorageBox storageBox = getStorageBox(productenToBePicked.get(productenToBePicked.size() - 1));
+        int x1 = storageBox.getX() + ((canvasWidth - (sizeBetween * 4)) / 5) / 2;
+        int y1 = storageBox.getY() + ((canvasHeight - (sizeBetween * 4)) / 5) / 2;
+        canvas.drawOval(x1 - 5, y1 - 5, 10, 10);
     }
 
     public StorageBox getStorageBox(Integer[] id) {
